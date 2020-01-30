7861cda9deea18c4d83cfe13ea2d914c7045a129
Jack Madden <jck13mad@gmail.com>
2020-01-30 01:02:16 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,7 +9,7 @@ def line(array)
       new_array.push("#{counter}. #{name}")
       counter += 1
     end
-    puts "The line is currently: #{nuarray.join(" ")}"
+    puts "The line is currently: #{new_array.join(" ")}"
   else
     puts "The line is currently empty."
   end
